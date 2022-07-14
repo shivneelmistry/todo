666326bab358a729baae6c01dd7ec18eb126ef74
Shivneel Mistry <shivneel@Shivneels-MacBook-Pro.local>
2022-07-14 02:14:35 -0400

    Fixed inconsistencies that prevented graphiql to run

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/your/endpoint"
   end
+  post "/graphql", to: "graphql#execute"
   # Defines the root path route ("/")
   # root "articles#index"
 end
