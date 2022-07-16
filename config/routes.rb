c5edaa5c43e042f0d543dd8d50f2dc0edc5c6523
Shivneel Mistry <shivneel@Shivneels-MacBook-Pro.local>
2022-07-15 21:31:07 -0400

    fixed minor issues with routing, and file naming [GraphiQL works correctly now]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/your/endpoint"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
   end
   post "/graphql", to: "graphql#execute"
   # Defines the root path route ("/")
